04d137747de4c1676fa4fa714a4ae5314a88a6af
BuildTools <alienufobomber@gmail.com>
2021-02-20 23:56:28 +0100

    v0.2

--- superhog_app/admin.py
+++ superhog_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Article, ArticleSection
+
+
+class ArticleSectionsInline(admin.StackedInline):
+    model = ArticleSection
+    extra = 1
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+    ]
+
+    inlines = [ArticleSectionsInline]
+
+admin.site.register(Article,ArticleAdmin)
+
+
 # Register your models here.
--- superhog_app/models.py
+++ superhog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length = 60)
+    publication_date = models.DateTimeField(auto_now_add=True, blank=True)
+
+
+
+class ArticleSection(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 60)
+    content = models.TextField(default="")
