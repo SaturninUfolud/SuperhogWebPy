96dbd3ef8ef8b8dfa27ecee3b85ae1dc3324ef31
BuildTools <alienufobomber@gmail.com>
2021-02-21 19:30:05 +0100

    v0.3c

--- superhog_app/models.py
+++ superhog_app/models.py
@@ -2,14 +2,45 @@ from django.db import models
 
 # Create your models here.
 
+
+class ExternalFileInfo(models.Model):
+    PICTURE = 0,
+    VIDEO_MP4 = 1,
+    TEXT = 2,
+    SOURCE_CODE = 3,
+    OTHER = 4
+
+    FILE_TYPE_CHOICES = [
+        (0, "Picture"),
+        (1, "Video"),
+        (2, "Text"),
+        (3, "Other")
+    ]
+
+    filetype = models.SmallIntegerField(choices = FILE_TYPE_CHOICES, default=PICTURE)
+    description = models.CharField(max_length = 45, default ="")
+    internal_path = models.CharField(max_length = 256)
+
+    def __str__(self):
+        if self.description is None or self.description == "":
+            return self.internal_path
+
+        else:
+            return self.internal_path + " (" + self.description +")"
+
+    @property
+    def get_url(self):
+        return "sh_app/" + self.internal_path
+
+
 class Article(models.Model):
     title = models.CharField(max_length = 60)
     publication_date = models.DateTimeField(auto_now_add=True, blank=True)
     basic_description = models.TextField(default="")
 
+
     def __str__(self):
         return self.title
-
 
 
 class ArticleSection(models.Model):
