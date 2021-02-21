236855ab3d726b4c4938bd2130eaf2ae37b81569
BuildTools <alienufobomber@gmail.com>
2021-02-21 18:22:29 +0100

    v0.3a

--- polls/views.py
+++ polls/views.py
@@ -21,16 +21,6 @@ from .models import Question, Choice
 #     return HttpResponse(template.render(context, request))
 
 
-
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})    
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
