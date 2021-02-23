f7032b9cdf68d632ae01b9ac3854e6801a2d4f20
BuildTools <alienufobomber@gmail.com>
2021-02-23 08:13:16 +0100

    v1.2

--- superhog_app/admin.py
+++ superhog_app/admin.py
@@ -3,15 +3,22 @@ from django.contrib import admin
 from .models import Article, ArticleSection, ExternalFileInfo
 
 
+
+
+
 class ArticleSectionsInline(admin.StackedInline):
     model = ArticleSection
     extra = 1
+
+
+
 
 class ArticleAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['title']}),
         (None, {'fields': ['article_type']}),
         (None, {'fields': ['basic_description']}),
+        (None, {'fields': ["gallery"]} ),
         
     ]
 
